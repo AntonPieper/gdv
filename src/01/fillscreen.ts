4c73b035e59496cafdfb174d17a2ca663afb7695
Anton Pieper <94118572+AntonPieper@users.noreply.github.com>
2023-04-18 12:50:12 +0000

    exercise 01

--- src/01/fillscreen.ts
+++ src/01/fillscreen.ts
@@ -12,6 +12,10 @@ import { swapBuffers } from './setup-fillscreen';
  * @param height The height of the canvas
  */
 export function fillscreen(data: Uint8ClampedArray, x: number, y: number, width: number, height: number) {
+    const index = (x + y * width) * 4;
+    data[index] = 0xff;
+    data[index + 3] = 0xff;
+    
     // TODO: Compute the position of pixel (x, y) in the linearised 'data' array. Each pixel is using 4 bytes in the data array, one each for red, green, blue and alpha.
     // TODO:  Set the red and alpha component of pixel (x, y) to maximum (255).
 }
