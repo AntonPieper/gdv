bfb79442ce7514a64333105c2abef57e6419336a
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2023-04-24 22:35:44 +0200

    refactor config

--- vite.config.ts
+++ vite.config.ts
@@ -1,95 +1,45 @@
 import { defineConfig } from "vite";
-import { resolve, dirname } from "path";
-import { fileURLToPath } from "url";
-import { existsSync, writeFile } from "fs";
+import { existsSync } from "fs";
 import glob from "fast-glob";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import exercises from "./exercises.json";
 
-const root = dirname(fileURLToPath(import.meta.url));
-
 const entryPoints: Record<string, string> = {};
 
-const setupScriptRegex = /src\/(\d\d)\/setup-(\w+).ts/;
-const setupScriptFiles = glob.sync("src/+([0-9])/setup-*.ts");
+for await (const file of glob.stream("src/*/setup-*.ts", { cwd: __dirname })) {
+  const nameStart = file.lastIndexOf("/setup-") + "/setup-".length;
+  const nameEnd = file.length - ".ts".length;
 
-const setupScriptAliases: Record<string, string> = {};
-
-for (const filename of setupScriptFiles) {
-  const match = setupScriptRegex.exec(filename);
-  if (!match) continue;
-
-  const num = match[1];
-  const name = match[2];
-
-  const setupScriptName = resolve(root, `src/${num}/setup-${name}.ts`);
-
-  if (existsSync(setupScriptName)) {
-    entryPoints[name] = resolve(root, `pages/${name}.html`);
-    setupScriptAliases[name] = setupScriptName;
-
-    console.log("+++++++ " + name + " => " + `./src/${num}/setup-${name}.ts`);
-  } else {
-    console.log("------- " + name + " does not exist.");
-  }
+  const name = file.toString().substring(nameStart, nameEnd);
+  if (name && existsSync(`${__dirname}/pages/${name}.html`)) {
+    entryPoints[name] = `${name}.html`;
+    console.log(`+++++++ ${name} => ${file}`);
+  } else console.warn(`------- ${name} has no HTML page.`);
 }
 
 for (const exercise of exercises) {
   for (const [name, entry] of Object.entries(exercise.entries)) {
-    if (name in setupScriptAliases) {
+    if (name in entryPoints) {
       entry["exists"] = true;
       exercise["exists"] = true;
     }
   }
 }
 
-const exerciseAliases = createExerciseAliases(setupScriptAliases);
-
-export default defineConfig({
-  plugins: [
-    ViteEjsPlugin({
-      exercises,
-    }),
-  ],
-  root: resolve(root, "pages"),
+export default defineConfig(() => ({
+  plugins: [ViteEjsPlugin(() => ({ exercises }))],
+  root: "pages",
   build: {
-    outDir: resolve(root, "dist"),
+    outDir: "../dist",
     rollupOptions: {
       input: entryPoints,
     },
   },
-  publicDir: resolve(root, "public"),
+  publicDir: "../public",
   resolve: {
     alias: {
-      ...exerciseAliases,
-      "/~": resolve(root, "node_modules"),
-      "/src": resolve(root, "src"),
+      "/node_modules": "../node_modules",
+      "/src": "../src",
     },
   },
-});
-
-function createExerciseAliases(setupScriptAliases: Record<string, string>) {
-  const aliasEntries = Object.entries(setupScriptAliases);
-  createTsconfig(aliasEntries);
-  return Object.fromEntries(
-    aliasEntries.map(([alias, file]) => [`/@${alias}`, file])
-  );
-}
-
-function createTsconfig(aliasEntries: [string, string][]) {
-  const tsConfigStart = `{
-  "compilerOptions": {
-    "paths": {
-      "/~/*": ["./node_modules/*"]`;
-  const tsConfigEnd = `
-    }
-  }
-}`;
-  const aliasTsconfig =
-    aliasEntries.reduce(
-      (result, [alias, file], i) =>
-        `${result},\n      "/@${alias}": ["${file}"]`,
-      tsConfigStart
-    ) + tsConfigEnd;
-  writeFile(resolve(root, ".aliases.tsconfig.json"), aliasTsconfig, () => {});
-}
+}));
